e9ef808b82fdf1051a9c2c268ff625b5a90f9b5d
AREWA TOLILOPE <arewatolulope5@gmail.com>
2025-06-23 09:33:04 +0100

    feat: Enhance navigation and destination details with improved styling and descriptions

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -21,8 +21,8 @@ function Nav() {
        <ul className='absolute font-BarlowCondensed lg:w-[50%] md:w-[80%] space-x-[15%] sm:space-x-[12%] text-[100%] p-8 pr-10 lg:pr-20 bg-gray-500/20 backdrop-blur-md right-0 z-0 justify-end items-center md:flex hidden'>
         <li
           onClick={() => navigate('/')}
-          className={`cursor-pointer hover:underline hover:underline-offset-35 hover:decoration-3 hover:decoration-gray-500 ${
-            isActive('/') ? 'underline underline-offset-35 decoration-3' : ''
+          className={`cursor-pointer hover:underline hover:underline-offset-35 hover:decoration-2 hover:decoration-gray-500 ${
+            isActive('/') ? 'underline underline-offset-35 decoration-2' : ''
           }`}
         >
           <span className='font-bold'>00</span> Home
@@ -43,7 +43,7 @@ function Nav() {
             isActive('/crew') ? 'underline underline-offset-35 decoration-3' : ''
           }`}
         >
-          <span className='font-bold'>02</span> Crew
+          <span className='font-bold'>03</span> Crew
         </li>
 
         <li
@@ -62,8 +62,7 @@ function Nav() {
         className="w-6 h-6 cursor-pointer block sm:hidden"
         onClick={() => setMenuOpen(true)} />
 
-        {menuOpen && (
-        <div className={`fixed top-0 right-0 z-50 flex flex-col items-start w-[70%] md:hidden h-full backdrop-blur-md bg-gray-500/20 p-8  space-y-[30%] transition-transform duration-300 ease-in-out transform ${menuOpen ? 'translate-x-0' : 'translate-x-[-100%]'}`}>
+        <div className={`fixed top-0 right-0 z-50 flex flex-col items-start w-[70%] md:hidden h-full backdrop-blur-md bg-gray-500/30 p-8  space-y-[30%] transition-transform duration-300 ease-in-out transform ${menuOpen ? 'translate-x-0' : 'translate-x-[100%]'}`}>
           <img
             src="/assets/shared/icon-close.svg"
             alt="Close"
@@ -71,13 +70,12 @@ function Nav() {
             onClick={() => setMenuOpen(false)}
           />
           <ul className="text-white space-y-8 text-left w-full font-BarlowCondensed">
-            <li onClick={() => { setMenuOpen(false); navigate('/') }}><span className='font-bold'>00</span> Home</li>
-            <li onClick={() => { setMenuOpen(false); navigate('/destination') }}><span className='font-bold'>01</span> Destination</li>
-            <li onClick={() => { setMenuOpen(false); navigate('/crew') }}><span className='font-bold'>02</span> Crew</li>
-            <li onClick={() => { setMenuOpen(false); navigate('/technology') }}><span className='font-bold'>03</span> Technology</li>
+            <li onClick={() => { navigate('/') }}><span className='font-bold'>00</span> Home</li>
+            <li onClick={() => { navigate('/destination') }}><span className='font-bold'>01</span> Destination</li>
+            <li onClick={() => { navigate('/crew') }}><span className='font-bold'>02</span> Crew</li>
+            <li onClick={() => { navigate('/technology') }}><span className='font-bold'>03</span> Technology</li>
           </ul>
         </div>
-      )}
  </nav>
   )
 
--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -3,10 +3,13 @@ import Nav from '../Components/Nav'
 
 
 const tabs = [
-    { name: 'Moon', image: './assets/destination/image-moon.png', distance: '384,400 km', travelTime: '3 DAYS' },
-    { name: 'Mars', image: './assets/destination/image-mars.png', distance: '225 mil. km', travelTime: '9 MONTHS' },
-    { name: 'Europa', image: './assets/destination/image-europa.png', distance: '628 mil. km', travelTime: '3 YEARS' },
-    { name: 'Titan', image: './assets/destination/image-titan.png', distance: '1.6 bil. km', travelTime: '7 YEARS' }
+    { name: 'Moon', description: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apolo 11 landing sites.", image: './assets/destination/image-moon.png', distance: '384,400 km', travelTime: '3 DAYS' },
+
+    { name: 'Mars', description: "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!", image: './assets/destination/image-mars.png', distance: '225 mil. km', travelTime: '9 MONTHS' },
+
+    { name: 'Europa',description:"The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabi", image: './assets/destination/image-europa.png', distance: '628 mil. km', travelTime: '3 YEARS' },
+
+    { name: 'Titan', description:"The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.", image: './assets/destination/image-titan.png', distance: '1.6 bil. km', travelTime: '7 YEARS' }
 ]
 
 function Destination() {
@@ -41,7 +44,7 @@ function Destination() {
                             </button>
                         ))}
                         </div>
-                        <div className='font-Bellefair text-[80px] md:text-[96px]'>MOON</div>
+                        <div className='font-Bellefair text-[80px] md:text-[96px] uppercase'>{tab.name}</div>
                         <div className='font-Barlow text-[18px] leading-relaxed mb-7 '>
                           {tab.description}
                         </div>
